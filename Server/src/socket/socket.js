4970c6c30bf0f63fe632611083d491a9497a2ca2
YashJha26 <142669810+YashJha26@users.noreply.github.com>
2024-10-13 18:00:41 +0530

    Update socket.js

--- Server/src/socket/socket.js
+++ Server/src/socket/socket.js
@@ -4,7 +4,12 @@ import {createServer} from 'node:http'
 const app = express();
 const server = createServer(app);
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://chatappfrontend-cvy9.onrender.com',
+    process.env.CLIENT_AUTH_URL || ''
+].filter(Boolean); 
+
 const io = new Server(server,{
     cors:{ 
         origin: allowedOrigins,
